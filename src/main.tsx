b15434499d0ae9636c21c5b7094e0c48778b0381
peds <hugh1127@gmail.com>
2025-01-15 16:31:17 +0900

    pref: optimize font

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.tsx'
+import nanumSquareNeoWoff from './assets/fonts/NanumSquareNeo-Variable.woff'
+import nanumSquareNeoWoff2 from './assets/fonts/NanumSquareNeo-Variable.woff2'
 import colors from './styles/colors.ts'
 
+// NODE: multiple @font-face is not supported on object styling
+// Change it to literal template if multiple @font-face is required
 const globalStyles = css({
+  '@font-face': {
+    fontFamily: '"NanumSquare Neo variable"',
+    src: `
+      local('NanumSquare Neo variable'),
+      url(${nanumSquareNeoWoff2}) format(woff2),
+      url(${nanumSquareNeoWoff}) format(woff)
+    `,
+    fontDisplay: 'swap',
+  },
   '*': {
     margin: 0,
     padding: 0,
@@ -13,7 +26,7 @@ const globalStyles = css({
     lineHeight: 1.5,
   },
   ':root': {
-    fontFamily: '"NanumSquare Neo", sans-serif',
+    fontFamily: '"NanumSquare Neo variable", system-ui, sans-serif',
     fontSynthesis: 'none',
     textRendering: 'optimizeLegibility',
     WebkitFontSmoothing: 'antialiased',
