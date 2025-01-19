68114ede98abd692b7a2d3c0ea2f7571d5c730df
devpeds <devpeds@users.noreply.github.com>
2025-01-19 14:46:40 +0000

    deploy: 91891f6221fbf3b720160d669a2376f123bcf0cb

---
+++ assets/beliefy-CSo_r6zj.js
@@ -0,0 +1,27 @@
+const n={},e=`# 빌리피(Beliefy)
+
+인증 기반 데이팅 서비스, 빌리피의 모바일 어플리케이션을 개발한 프로젝트입니다. 프로필 심사, 이성 추천 및 매칭, 채팅, 인증 등의 기능을 포함하고 있습니다.
+
+## Overview
+
+- **Period**: 2023. 10. ~ 2024. 11.
+- **Members**: 프론트엔드 1명, 풀스택 2명, 백엔드 2명
+- **Role**: 프론트엔드 개발
+- **TechStack**:
+  - Dart
+  - Flutter
+  - BloC
+  - Firebase
+  - Fastlane
+
+## Contributions
+
+- BLoC 패턴을 활용한 앱 내 주요 기능 및 화면 개발
+- Fastlane, Github Workflow를 활용한 배포 관리
+
+## Significance
+
+처음으로 플러터로 개발한 프로젝트입니다. 프로젝트를 진행하면서 플러터와 React Native의 차이점과 각 프레임워크의 장단점을 직접 체감할 수 있었습니다.
+
+또한, BloC 패턴을 활용하면서 Flutter 생태계에서의 상태 관리 방식을 경험할 수 있었습니다. 그리고 프레젠테이션 레이어와 비즈니스 로직을 명확히 분리하는 BloC 패턴의 구조적 이점 덕분에 유지보수성과 코드의 가독성을 높일 수 있음을 느꼈습니다.
+`;export{n as attributes,e as markdown};
---
+++ assets/proofy-Bm142fTi.js
@@ -0,0 +1,32 @@
+const n={},t=`# 프루피(Proofy)
+
+소울바운드토큰(SBT) 기반 신원 인증 서비스 '프루피'의 모바일 어플리케이션을 개발한 프로젝트입니다. KYC와 SBT 민팅을 통한 신원인증 기능, 지갑 관리, 연봉 비교 기능 등을 포함하고 있습니다.
+
+## Overview
+
+- **Period**: 2023. 07. ~ 2023. 10.
+- **Members**: 프론트엔드 개발자 2명, 백엔드 개발자 1명
+- **Role**: 프론트엔드 개발
+- **TechStack**:
+  - TypeScript
+  - React Native
+  - TailwindCSS(Nativewind)
+  - Tanstack Query
+  - Jotai
+
+## Contributions
+
+- 앱 주요 기능 개발
+  - react-hook-form을 이용한 KYC 입력 폼 개발
+  - 소셜 로그인(Kakao, Google, Apple) 연동
+  - 소득 인증 기능 개발
+  - react-native-reanimated을 활용한 애니메이션 추가
+- Fastlane, Github Workflow를 이용한 배포 관리
+
+## Significance
+
+이 프로젝트에서 Git-flow 전략을 처음 경험했습니다. 이전 회사에서는 트렁크 기반 개발 환경에서 하나의 main 브랜치만 사용하여 개발했지만, 이번 프로젝트에서는 Git-flow를 도입하면서 두 전략의 차이점과 각 접근 방식의 장단점을 비교할 수 있었습니다.
+트렁크 기반 개발은 작은 변경사항을 자주 통합할 수 있어 브랜치 관리가 간편하고, 코드 리뷰 부담이 줄어든다는 장점이 있습니다. 하지만 PR당 코드량이 적기 때문에 모든 기능을 하나의 PR에 담기 어려워, 코드 리뷰 시 기능의 전체 흐름을 이해하기 까다로운 경우가 있었습니다. 반면, Git-flow 전략에서는 기능 단위로 브랜치와 PR을 생성하기 때문에 코드 리뷰 시 기능의 맥락을 더 쉽게 파악할 수 있고, 완성된 기능만 통합할 수 있어 배포 관리를 더 안정적으로 할 수 있었습니다.
+
+또한, Jotai를 처음 사용한 프로젝트이기도 합니다. SWR과 Tanstack Query의 등장 이후 클라이언트 상태와 서버 상태가 분리되면서 전역 상태로 관리할 필요성이 줄어들었습니다. 이번 프로젝트에서는 전역 상태 라이브러리로 Redux 대신 Jotai를 사용하면서 보일러플레이트와 번들 크기를 줄일 수 있었습니다. 또한, Context API와 달리 Jotai는 상태를 소비하는 모든 컴포넌트가 아닌 특정 부분에만 리렌더링을 발생시킬 수 있어 성능을 더욱 개선할 수 있었습니다.
+`;export{n as attributes,t as markdown};
