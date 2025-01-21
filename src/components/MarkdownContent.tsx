c5961c55c5d9bc3b9d0763749c2e1891354e12b6
peds <hugh1127@gmail.com>
2025-01-21 12:45:34 +0900

    refactor: migrate react to 19

--- src/components/MarkdownContent.tsx
+++ src/components/MarkdownContent.tsx
@@ -1,6 +1,5 @@
-import { ReactElement } from 'react'
+import { ReactElement, use } from 'react'
 import Markdown from 'react-markdown'
-import { use } from 'react18-use'
 import rehypeHighlight from 'rehype-highlight'
 import rehypeRaw from 'rehype-raw'
 import 'highlight.js/styles/github.min.css'
