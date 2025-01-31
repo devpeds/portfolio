8e667cc42316cc5aa62677f935856a3ef908dc60
peds <hugh1127@gmail.com>
2025-02-01 00:32:19 +0900

    fix: open project url on new tab

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,34 @@
 import react from '@vitejs/plugin-react-swc'
 import hljs from 'highlight.js'
 import MarkdownIt from 'markdown-it'
+import markdownItAttrs from 'markdown-it-attrs'
 import { defineConfig } from 'vite'
 import imagemin from 'vite-plugin-imagemin'
 import { Mode, plugin as markdown } from 'vite-plugin-markdown'
 import svgr from 'vite-plugin-svgr'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
+const markdownIt = MarkdownIt({
+  html: true,
+  highlight: (str, lang) => {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(str, { language: lang }).value
+      } catch (err) {
+        console.warn(err)
+      }
+    }
+
+    return ''
+  },
+}).use(markdownItAttrs)
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tsConfigPaths(),
     svgr(),
-    markdown({
-      mode: [Mode.HTML],
-      markdownIt: MarkdownIt({
-        html: true,
-        highlight: (str, lang) => {
-          if (lang && hljs.getLanguage(lang)) {
-            try {
-              return hljs.highlight(str, { language: lang }).value
-            } catch (err) {
-              console.warn(err)
-            }
-          }
-
-          return ''
-        },
-      }),
-    }),
+    markdown({ mode: [Mode.HTML], markdownIt }),
     imagemin({ webp: {} }),
     react({
       jsxImportSource: '@emotion/react',
