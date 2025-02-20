552568910ad4b43c98f431adcdbc9bf4a7a8a4e9
peds <hugh1127@gmail.com>
2025-02-20 20:23:43 +0900

    contents: add showcase images on beliefy

--- src/assets/images/projects/showcase.ts
+++ src/assets/images/projects/showcase.ts
@@ -3,6 +3,10 @@ import altong2 from './altong-2.png?preset=showcase'
 import altong3 from './altong-3.png?preset=showcase'
 import altong4 from './altong-4.png?preset=showcase'
 import altong5 from './altong-5.png?preset=showcase'
+import beliefy1 from './beliefy-1.png?preset=showcase'
+import beliefy2 from './beliefy-2.png?preset=showcase'
+import beliefy3 from './beliefy-3.png?preset=showcase'
+import beliefy4 from './beliefy-4.png?preset=showcase'
 import playwingsWeb1 from './playwings-web-1.png?preset=showcase'
 import playwingsWeb2 from './playwings-web-2.png?preset=showcase'
 import playwingsWeb3 from './playwings-web-3.png?preset=showcase'
@@ -23,6 +27,12 @@ const showcase = {
     { image: altong3, alt: '요금제 상세' },
     { image: altong4, alt: '(모바일) 홈 & 요금제 검색' },
     { image: altong5, alt: '(모바일) 검색 필터 & 요금제 상세' },
+  ],
+  beliefy: [
+    { image: beliefy1, alt: '빌리픽' },
+    { image: beliefy2, alt: '호감' },
+    { image: beliefy3, alt: '프로필 상세' },
+    { image: beliefy4, alt: '매칭 성공' },
   ],
   proofy: [
     { image: proofy1, alt: '홈 화면 & KYC' },
